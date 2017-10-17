cdbdb6cd5f866dce18d20dd05c13286309dccc2b
absolitudo <absolitudo@gmail.com>
2017-10-17 13:31:58 +0200

    updated professions.json, new routes for professions

--- professions/routes.js
+++ professions/routes.js
@@ -12,5 +12,32 @@ router.get('/blacksmithing', (req, res) => {
     res.sendFile(__dirname + '/blacksmithing.json')
 })
 
+router.get('/enchanting', (req, res) => {
+    res.sendFile(__dirname + '/enchanting.json')
+})
+
+router.get('/engineering', (req, res) => {
+    res.sendFile(__dirname + '/engineering.json')
+})
+
+router.get('/inscription', (req, res) => {
+    res.sendFile(__dirname + '/inscription.json')
+})
+
+router.get('/jewelcrafting', (req, res) => {
+    res.sendFile(__dirname + '/jewelcrafting.json')
+})
+
+router.get('/leatherworking', (req, res) => {
+    res.sendFile(__dirname + '/leatherworking.json')
+})
+
+router.get('/mining', (req, res) => {
+    res.sendFile(__dirname + '/mining.json')
+})
+
+router.get('/tailoring', (req, res) => {
+    res.sendFile(__dirname + '/tailoring.json')
+})
 
 module.exports = router
