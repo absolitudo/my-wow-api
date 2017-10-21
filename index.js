e2f73b2df20f2ee0b5c8d1500cf26ce3430f1c25
absolitudo <absolitudo@gmail.com>
2017-10-21 20:07:19 +0200

    enable cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express')
+var cors = require('cors')
 var app = express()
 
 /* Routes */
 var professionRoutes = require('./professions/routes')
-
+app.use('/professions', cors())
 app.use('/professions', professionRoutes)
 
 app.get('/', (req, res) => {
     res.send('This is my own api server for some wow stuff because i could\'nt find anything that would provide me the data that i need' )
 })
  
-app.listen(process.env.PORT || 3000, () => console.log('server up'))
+app.listen(process.env.PORT || 7777, () => console.log('server up'))
