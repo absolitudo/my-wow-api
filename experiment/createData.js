d6b8600d1e9c5d4984c8e9b6caece2c105b4f69c
absolitudo <absolitudo@gmail.com>
2017-11-19 11:10:43 +0100

    get item name, id, quantity

--- experiment/createData.js
+++ experiment/createData.js
@@ -4,16 +4,47 @@ let http = require('http')
 let alchemy = {}
 
 http.get('http://mop-shoot.tauri.hu/?spell=114773', function(res) {
+
+    
+
+    let data = ''
+
     res.on('data', chunk => {
         chunk = chunk.toString()
+        data += chunk
 
-        /* get reguired profession level of the recipe */
-        let levelRegExp = /\d+/.exec(/Requires \w+ \(\d+\)/g.exec(chunk))
-        levelRegExp && console.log(+levelRegExp[0])
-
+        
 
 
     })
+
+    res.on('end', (something) => {
+
+        let level
+        let name
+        let itemID
+        let itemQuantity
+
+        /* Get reguired profession level of the recipe */
+        level = +/\d+/.exec(/Requires \w+ \(\d+\)/g.exec(data))[0]
+
+        /* Get the name of the item or recipe */
+        let nameRegExp = /<h1>[\w ']+<\/h1>/g.exec(data)
+        name = nameRegExp[0].slice(4, nameRegExp.length - 6)
+
+        /* Get the ID of the item */
+        let itemIDRegExp = new RegExp('<a href="\\?item=\\d+">' + name,'g')
+        itemIDRegExp = /\d+/.exec(itemIDRegExp.exec(data))
+        itemID = +itemIDRegExp[0]
+
+        /* Get item quantity */
+        let itemQuantityRegExp = new RegExp('createIcon\\(' + itemID + ', \\d+, \\d+\\)')
+        itemQuantity = /\d+\)$/.exec(itemQuantityRegExp.exec(data))[0]
+        itemQuantity = +itemQuantity.slice(0, itemQuantity.length - 1)
+        
+        console.log(level, name, itemID, itemQuantity)
+    })
+
 }).on('error', function(e) {
     console.log("Got error: " + e.message);
 })
