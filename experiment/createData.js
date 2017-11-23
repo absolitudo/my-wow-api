593f24fa3135f9f9988421d800faee4d1b793b3f
absolitudo <absolitudo@gmail.com>
2017-11-23 15:15:43 +0100

    cleanup, ready to get data

--- experiment/createData.js
+++ experiment/createData.js
@@ -1,8 +1,7 @@
 const request = require('request')
 const fs = require('fs')
 const http = require('http')
-/* TODO: */
-/* GET ALL THE PROFESSION DATA */
+
 /* Downloads icon */
 const getIcon = (iconName) => {
     return new Promise((resolve, reject) => {
@@ -39,6 +38,7 @@ const getData = (ID, isSpell) => {
     })
 }
 
+/* Gets item quantity */
 const getQuantityFromItemId = (id, data) => {
     let itemQuantityRegExp = new RegExp('createIcon\\(' + id + ', \\d+, \\d+\\)')
     let quantity = /\d+\)$/.exec(itemQuantityRegExp.exec(data)) ? +/\d+\)$/.exec(itemQuantityRegExp.exec(data))[0].replace(/\)/, '') : 1
@@ -84,76 +84,8 @@ const getReagentTooltip = async (ID, name) => {
     })
 }
 
-/* Returns the information of an item from spell id */
-const getItemInfoFromSpellID = async (spell) => {
-    let item = {}
-    let data = await getData(ID, true)
-    /* Get reguired profession level of the recipe */
-    item.profReq = +/\d+/.exec(/Requires \w+ \(\d+\)/g.exec(data))[0]
-
-    /* Get the name of the item or recipe */
-    let nameRegExp = /<h1>[\w ']+<\/h1>/g.exec(data)[0]
-    item.name = nameRegExp.replace(/<(.*?)>/g, '')
-
-    /* Get the ID of the item */
-    let itemIDRegExp = new RegExp('<a href="\\?item=\\d+">' + item.name,'g')
-    itemIDRegExp = /\d+/.exec(itemIDRegExp.exec(data))[0]
-    item.id = +itemIDRegExp
-
-    /* Get item quantity */
-    let itemQuantityRegExp = new RegExp('createIcon\\(' + item.id + ', \\d+, \\d+\\)')
-    item.quantity = /\d+\)$/.exec(itemQuantityRegExp.exec(data))[0]
-    item.quantity = +item.quantity.replace(/\)/, '')
-    
-    /* Get iconName */
-    let itemObjectRegExp = new RegExp('_\\[' + item.id + '\\](.+?)}')
-    let iconNameRegExp = /icon:(.*?),/.exec(itemObjectRegExp.exec(data)[0])[0]
-    item.iconName = iconNameRegExp.replace(/(icon:|,|')/g, '').toLocaleLowerCase()
-
-    /* Get icon */
-    if(!doesFileExist('./icons/' + item.iconName + '.png')) {
-        getIcon(item.iconName + '.png')
-    } 
-
-    /* Get item quality */
-    let itemQualityRegExp = /quality:\d+/.exec(itemObjectRegExp.exec(data)[0])
-    item.quality = +/\d+/.exec(itemQualityRegExp[0])[0]
-     
-    /* Get tooltip */
-    let tooltipRegExp = /<table class="tooltip-t"(.*)/.exec(data)[0]
-    item.tooltip = getTooltip(tooltipRegExp, item.name)
-
-    /* Get reagents */
-    let reagentsRegExp = /Reagents:(.*?)<br>/.exec(data)
-    item.reagents = await Promise.all(reagentsRegExp[0]
-        .replace(/(Reagents: |<(.*?)>|\(|\))/g, '')
-        .split(',')
-        .map(async (reagent) => {
-            let reagentName = reagent.replace(/\d+/g, '').trim()
-            let reagentIDRegExp = new RegExp('item=\\d+">' + reagentName)
-            let reagentID = +/\d+/.exec(reagentIDRegExp.exec(data)[0])[0]
-            let reagentObjectRegExp = new RegExp('_\\[' + reagentID + '\\]' + '(.*?)};')
-            reagentObjectRegExp = reagentObjectRegExp.exec(data)[0]
-            let reagentIconName = /icon:'(.*?),/.exec(reagentObjectRegExp)[0].replace(/(icon:|'|,)/g, '').toLocaleLowerCase()
-            
-            if(!doesFileExist('./icons/' + reagentIconName + '.png')) {
-                await getIcon(reagentIconName + '.png')
-            }
-
-
-            return {
-                name: reagentName,
-                id: reagentID,
-                tooltip: await getReagentTooltip(reagentID, reagentName),
-                iconName: reagentIconName,
-                quality: +/\d+/.exec(/quality:\d+/.exec(reagentObjectRegExp)[0])[0],
-                quantity: /\d+/.exec(reagent) ? +/\d+/.exec(reagent)[0] : 1
-            }
-        }))
-    return item
-}
-
-const alternativeScrape = async (ID, spell) => {
+/* Gets the information of the item */
+const getItemInfo = async (ID, spell) => {
     let item = {}
     let data = await getData(ID, true)
 
@@ -167,28 +99,22 @@ const alternativeScrape = async (ID, spell) => {
 
     /* Get reguired profession level of the recipe */
     item.profReq = /Requires \w+ \(\d+\)/g.exec(data) ? +/\d+/.exec(/Requires \w+ \(\d+\)/g.exec(data))[0] : undefined
-    console.log('profession requirement:', item.profReq)
 
     /* Get the object of the item on the page to get more info from it */
     let itemObject = /_\[\d+\]={icon:'(.*?)',name_enus:'(.*?)'.quality:\d+}/.exec(data)[0]
-    console.log(itemObject)
 
     /* Get the item id */
     item.id = +/\d+/.exec(itemObject)[0]
-    console.log('item id:', item.id)
 
     /* Get item quantity */
     item.quantity = getQuantityFromItemId(item.id, data)
-    console.log('item quantity:', item.quantity)
 
     /* Get item quality */
     item.quality = +/\d+}$/.exec(itemObject)[0].replace('}', '')
-    console.log('item quality:', item.quality)
 
     /* Get tooltip */
     let tooltipRegExp = /<table class="tooltip-t"(.*)/.exec(data)[0]
     item.tooltip = getTooltip(tooltipRegExp, item.name)
-    console.log('item tooltip:', item.tooltip)
     
     /* Get reagents */
     let reagentsRegExp = /<table class="iconlist">([\s\S]+?|.*?)<\/table>/.exec(data)
@@ -219,22 +145,17 @@ const alternativeScrape = async (ID, spell) => {
             })
         )
     return item
-    console.log('item reagents:', item.reagents)
 }
 
 async function main() {
     
 
-    let profession = require('./enchanting.json')
+    let profession = require('./alchemy.json')
     let newProfession = {}
-    let enchantItem = await alternativeScrape(7776, profession[7776])
-    console.log(enchantItem)
-    // PROBABLY THE ITEM NAME IS CAUSING ALL THE FKING ISSUES
-    /*
     for(let spellID in profession) {
         try {
             console.log(spellID, 'is processing')
-            let item = await getItemInfoFromSpellID(spellID)
+            let item = await getItemInfo(28584, profession[28584])
             newProfession[item.name] = item
             console.log(spellID, item.name, 'is processed')
         } catch(e) {
@@ -254,7 +175,6 @@ async function main() {
             console.log('done')
         }
     })
-    */
 }
 
 main()
